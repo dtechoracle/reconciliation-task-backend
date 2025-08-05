9f0499cb60b0f0aaa91390d757cd60f980d91f48
dtechoracle <iamezekieljeremiah@gmail.com>
2025-08-05 07:42:20 +0100

    fixed CORS error

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: 'http://localhost:https://reconciliation-task-frontend.vercel.app',
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
